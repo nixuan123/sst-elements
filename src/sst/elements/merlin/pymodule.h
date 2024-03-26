b8f5fd6a767ab58c55c766a69ca8b509480ee925
nixuan123 <128340967+nixuan123@users.noreply.github.com>
2024-03-26 19:16:00 +0800

    Update pymodule.h

--- src/sst/elements/merlin/pymodule.h
+++ src/sst/elements/merlin/pymodule.h
@@ -15,14 +15,22 @@
 // information, see the LICENSE file in the top level directory of the
 // distribution.
 
-
+#总的来说这段代码定义了一个C接口，允许C或C++代码通过genMerlinPyModule函数与
+#Python模块进行交互，这种技术通常用于扩展Python的功能，或者在C/C++项目中嵌入
+#Python脚本
 #ifndef COMPONENTS_MERLIN_PYMODULE_H
 #define COMPONENTS_MERLIN_PYMODULE_H
 
 #ifdef __cplusplus
+#检查是否在C++环境中编译
+
+#用于告诉编译器在这个{}块内的函数使用C语言
+#的链接方式。这样做的目的是让C++编译器生成
+#与C语言兼容的函数名，以便C代码可以链接这些函数。
 extern "C" {
 #endif
 
+#这个函数的目的是加载Merlin Python模块。函数的具体实现在pymodule.cc文件中
 void* genMerlinPyModule(void);
 
 #ifdef __cplusplus
