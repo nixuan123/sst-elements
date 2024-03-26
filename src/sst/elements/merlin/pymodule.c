311178c4f57b96a8fb56037bd5ac39e73f584d1a
nixuan123 <128340967+nixuan123@users.noreply.github.com>
2024-03-26 19:50:49 +0800

    Update pymodule.c

--- src/sst/elements/merlin/pymodule.c
+++ src/sst/elements/merlin/pymodule.c
@@ -12,12 +12,15 @@
 // This file is part of the SST software package. For license
 // information, see the LICENSE file in the top level directory of the
 // distribution.
-
+#sst_config.h文件是SST核心配置文件
 #include <sst_config.h>
+#Python.h文件时Python解释器的C接口头文件
 #include <Python.h>
 
 #include "pymodule.h"
 
+#定义了一个静态字符数组pymerlin，其中包含了Python代码的字节序列
+#这个数组可能包含Python模块的源代码(pymerlin.inc)，它将被编译成Python模块
 static char pymerlin[] = {
 #include "pymerlin.inc"
     0x00};
@@ -25,8 +28,10 @@ static char pymerlin[] = {
 void* genMerlinPyModule(void)
 {
     // Must return a PyObject
-
+    #将pymerlin数组中的Python代码编译成一个PyObject对象，这个对象代表了编译后的Python代码
+    #"pymerlin"是提供的文件名，Py_file_input表示代码类型是文件输入
     PyObject *code = Py_CompileString(pymerlin, "pymerlin", Py_file_input);
+    #最后，使用PyImport_ExecCodeModule函数执行编译后的代码，并创建一个新的模块对象
     return PyImport_ExecCodeModule("sst.merlin", code);
 }
 
